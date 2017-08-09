45c1f22e2594637dc9f116ab7ebc3aa3affd1448
Jason Maurer <j@maur.co>
2017-08-09 16:46:28 -0600

    check for redirects

--- src/index.js
+++ src/index.js
@@ -1,6 +1,32 @@
+import redirect from './redirect'
+
 module.exports = (opts = {}) => {
-  return (event, context, callback) => {
-    const { request } = event.Records[0].cf
-    callback(null, request)
+  return async (event, context, callback) => {
+    try {
+      const { request } = event.Records[0].cf
+
+      opts.redirects = opts.redirects || []
+
+      const redirect = parseRedirects(request, opts)
+      if (redirect) return callback(null, redirect)
+
+      callback(null, request)
+    } catch (err) {
+      console.error('Error:', err.message || err)
+      callback(err)
+    }
+  }
+}
+
+function parseRedirects (req, opts) {
+  const redirects = Array.isArray(opts.redirects)
+    ? opts.redirects
+    : Object.keys(opts.redirects)
+      .map((i) => ({ from: i, to: opts.redirects[i] }))
+
+  for (const data of redirects) {
+    if (req.uri === data.from) {
+      return redirect(data.to, data.code)
+    }
   }
 }
--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,42 @@
 import { expect } from 'chai'
+import sinon from 'sinon'
+import lambchop from './index'
 
 describe('unit: index', () => {
-  it('runs first test', async () => {
-    expect(1 + 1).to.equal(2)
+  const config = {
+    redirects: [
+      { from: '/hi', to: '/hey' },
+      { from: '/hello', to: '/whatup', code: 302 }
+    ]
+  }
+
+  it('without anything', async () => {
+    const spy = sinon.spy()
+    const request = { uri: '/' }
+    const evt = { Records: [{ cf: { request } }] }
+    lambchop(config)(evt, null, spy)
+    expect(spy.calledWith(null, request)).to.equal(true)
+  })
+
+  it('redirects (array)', async () => {
+    const spy = sinon.spy()
+    const request = { uri: '/hello' }
+    const evt = { Records: [{ cf: { request } }] }
+    lambchop(config)(evt, null, spy)
+    expect(spy.args[0][1].status).to.equal('302')
+    expect(spy.args[0][1].headers.location[0].value).to.equal('/whatup')
+  })
+
+  it('redirects (object)', async () => {
+    const spy = sinon.spy()
+    const request = { uri: '/hello' }
+    const evt = { Records: [{ cf: { request } }] }
+    lambchop({
+      redirects: {
+        '/hello': '/whatup'
+      }
+    })(evt, null, spy)
+    expect(spy.args[0][1].status).to.equal('301')
+    expect(spy.args[0][1].headers.location[0].value).to.equal('/whatup')
   })
 })
