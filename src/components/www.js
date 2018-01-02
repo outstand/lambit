fb01053d14627c1436438a6f88f36692679018a1
Ryan Schlesinger <ryan@ryanschlesinger.com>
2018-01-02 10:10:13 -0800

    www redirect assumes https protocol

--- src/components/www.js
+++ src/components/www.js
@@ -19,12 +19,12 @@ export default function (req, res, toWww) {
   /* redirect to host with www */
   if (toWww && !PATTERN_WWW.test(host)) {
     const newHost = `www.${host}`
-    return Object.assign(res, redirect(`${newHost}${req.uri}`))
+    return Object.assign(res, redirect(`https://${newHost}${req.uri}`))
   }
 
   /* redirect to host without www */
   if (!toWww && PATTERN_WWW.test(host)) {
     const newHost = host.replace(PATTERN_WWW, '')
-    return Object.assign(res, redirect(`${newHost}${req.uri}`))
+    return Object.assign(res, redirect(`https://${newHost}${req.uri}`))
   }
 }
